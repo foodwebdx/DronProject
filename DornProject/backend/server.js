85db3a457b1a928e2e91ee5c3c8e95d4b9ae92e3
foodwebdx <santifernandez0902@gmail.com>
2024-11-16 12:25:39 -0500

    Backed deployed

---
+++ DornProject/backend/server.js
@@ -0,0 +1,19 @@
+const exppress = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
+const app = exppress();
+app.use(exppress.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "shopro"
+})
+
+
+app.listen(8081,() => {
+    console.log("Listening ");
+})
