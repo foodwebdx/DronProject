3f8ecab4f3e8c0aabde39c98ee7563adfa561dcd
Victor Cely <victorcely@iMac-de-Victor.local>
2024-11-18 22:08:10 -0500

    fucked up reservas conficltos no sabia

--- DornProject/frontend/src/components/FormularioReserva.jsx
+++ DornProject/frontend/src/components/FormularioReserva.jsx
@@ -9,51 +9,6 @@ const FormularioReserva = () => {
 
     const [fechaReserva, setFechaReserva] = useState('');
     const [error, setError] = useState('');
-    const [animar, setAnimar] = useState(false);
-
-    useEffect(() => {
-        if (animar) {
-            const generateRain = () => {
-                const rainContainer = document.querySelector('.rain');
-                for (let i = 0; i < 100; i++) {
-                    const drop = document.createElement('div');
-                    drop.classList.add('drop');
-                    drop.style.left = `${Math.random() * 100}vw`;
-                    drop.style.animationDuration = `${Math.random() * 2 + 0.5}s`;
-                    drop.style.animationDelay = `${Math.random() * 2}s`;
-                    rainContainer.appendChild(drop);
-                }
-            };
-
-            generateRain();
-        }
-
-        setTimeout(() => {
-            setAnimar(false);
-        }, 5000);
-    }, [animar]);
-
-    const handleCheckWeather = async () => {
-        try {
-            const res = await fetch(`${api.base}weather?q=${ciudad}&units=metric&appid=${api.key}`);
-            const data = await res.json();
-    
-            if (data.cod === 200) {
-                const currentWeather = data.weather[0].description.toLowerCase();
-                console.log('Weather description:', currentWeather); // Mostrar en la consola
-                setClima(currentWeather);
-                return currentWeather;
-            } else {
-                setError('Ciudad no encontrada. Verifica el nombre e inténtalo de nuevo.');
-                return null;
-            }
-        } catch (err) {
-            console.error(err);
-            setError('Error al obtener el clima. Intenta de nuevo más tarde.');
-            return null;
-        }
-    };
-    
 
     const handleSubmit = async (e) => {
         e.preventDefault();
